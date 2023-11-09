73f9e5a88cf7e8418dcdbd1f48833b86285e085e
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-11-09 22:20:08 +0900

    6 정렬까지 완
    료

---
+++ COTE/20231109/6_selection_sort_prac2.py
@@ -0,0 +1,13 @@
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i+1, len(array)):
+        if(array[min_index] > array[j]):
+            min_index = j
+
+    # 뒤에꺼가 더 작으면 바뀌는것, 크면 걍 그대로
+    # 시간복잡도 (N^2 + N )/2
+    array[i],array[min_index] = array[min_index], array[i]
+
+print(array)
